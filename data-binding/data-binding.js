42f55adf474a732c137f5ed820890626e92c6efe
kewon96 <yang0184@naver.com>
2021-04-07 23:55:37 +0900

    v-bind

--- data-binding/data-binding.js
+++ data-binding/data-binding.js
@@ -2,11 +2,19 @@ new Vue({
     el: '#app',
     data: {
         number: 10,
-        uuid: 'n1',
+        uuid: 'abc1234',
+        cName: 'text-bold',
+        loading: true,
+        message: '',
+        checked: true,
     },
     computed: {
         doubleNumber: function() {
             return this.number * 2;
+        },
+        checkedConvert: function() {
+            return this.checked ? 1 : 0;
         }
+
     }
 })
