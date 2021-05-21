01af7973480ff2d39bd53792e2e86a9c9a8ae83d
Kewon <yang0184@naver.com>
2021-05-21 15:06:34 +0900

    vue-form
    간단한 From을 통해 서버로 데이터 전송하기

---
+++ vue-form/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="password">password: </label>
+        <input id="password" type="password" v-model="password">
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault(); // 이벤트취소 -> 새로고침이 진행되지 않음
+      console.log(this.username, this.password);
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+
+      axios.post(url, data)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
